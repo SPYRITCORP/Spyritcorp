c7a892ab1008be601f315f4395fa7a007b55aaa5
SPYRITCORP <kiritonikens9@gmail.com>
2025-06-03 19:56:44 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const botonCambiar = document.getElementById("botonCambiar");
-if (botonCambiar) {
-  botonCambiar.addEventListener("click", function () {
-    const texto = document.querySelector(".texto-centro p");
-    if (texto) {
-      texto.textContent = "SPYRYTCORP te da la vienvenida";
-    }
-  });
-}
